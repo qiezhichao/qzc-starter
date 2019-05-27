4de58e37cc2372e872494aff1dce95e5a79986b3
qiezhichao <zhichao_qzc@sina.com>
2019-05-27 20:25:24 +0800

    update start.sh

--- qzc-starter-parent/start.sh
+++ qzc-starter-parent/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-nohup java -jar -Xms512m -Xmx2048m -XX:PermSize=1024m -XX:MaxNewSize=108m -XX:MaxPermSize=2048m ./xingdadata.jar -Dfile.encoding=UTF-8 --spring.config.location=/home/qzc/xingda/config/application.yml --server.port=8999 >/dev/null 2>&1 &
+PORT=8989
+SERVICENAME=xingdadata.jar
+SPRINGCONFIG=/home/qzc/xingda/config/application.yml
+
+nohup java -jar -Xms512m -Xmx2048m -XX:PermSize=1024m -XX:MaxNewSize=108m -XX:MaxPermSize=2048m ./$SERVICENAME -Dfile.encoding=UTF-8 --spring.config.location=$SPRINGCONFIG --server.port=$PORT >/dev/null 2>&1 &
