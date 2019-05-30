db23ea916281bc94be8683da5398724843c0e51f
qiezhichao <zhichao_qzc@sina.com>
2019-05-30 21:31:46 +0800

    新增腾讯云cos服务

---
+++ qzc-base-service/src/main/java/com/qzc/updownload/TXCosService.java
@@ -0,0 +1,14 @@
+package com.qzc.updownload;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ *  腾讯cos服务
+ * @Author:         qiezhichao
+ * @CreateDate:     2019/5/30 21:04
+ */
+@Service
+@Slf4j
+public class TXCosService {
+}
