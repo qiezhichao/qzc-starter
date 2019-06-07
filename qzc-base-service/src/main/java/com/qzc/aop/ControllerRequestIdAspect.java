264776b636e19584e7431acf9cb4096a5863e694
qiezhichao <zhichao_qzc@sina.com>
2019-06-07 09:54:20 +0800

    requestId横切点新增异常处理逻辑

--- qzc-base-service/src/main/java/com/qzc/aop/ControllerRequestIdAspect.java
+++ qzc-base-service/src/main/java/com/qzc/aop/ControllerRequestIdAspect.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * Controller请求id切面类
@@ -25,11 +26,16 @@ public class ControllerRequestIdAspect {
 
     // 横切点，哪些方法需要被横切
     @Pointcut("execution(public * com..*.*Controller.*(..))")
-    public void addRequestId() {
+    public void addControllerRequestId() {
+    }
+
+    // 横切点，哪些方法需要被横切
+    @Pointcut("execution(public * com..*.*ExceptionHandler.*(..))")
+    public void addExceptionHandlerRequestId() {
     }
 
     @AfterReturning(value = "execution(public * com..*.*Controller.*(..))", returning = "result")
-    public void afterReturning(JoinPoint joinPoint, Object result) {
+    public void afterControllerReturning(JoinPoint joinPoint, Object result) {
         try {
             RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
             //从获取RequestAttributes中获取HttpServletRequest的信息
@@ -38,6 +44,21 @@ public class ControllerRequestIdAspect {
 
             Response response = (Response) result;
             response.setRequestId(requestId);
+        } catch (Exception e) {
+            log.error("AOP after printLog error!", e);
+        }
+    }
+
+    @AfterReturning(value = "execution(public * com..*.*ExceptionHandler.*(..))", returning = "result")
+    public void afterExceptionHandlerReturning(JoinPoint joinPoint, Object result) {
+        try {
+            RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+            //从获取RequestAttributes中获取HttpServletRequest的信息
+            HttpServletRequest request = (HttpServletRequest) requestAttributes.resolveReference(RequestAttributes.REFERENCE_REQUEST);
+            String requestId = String.valueOf(request.getAttribute("requestId"));
+
+            Map map = (Map) result;
+            map.put("requestId", requestId);
         } catch (Exception e) {
             log.error("AOP after printLog error!", e);
         }
