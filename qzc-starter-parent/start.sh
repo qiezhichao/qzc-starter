982f735a5950fff1d79450728402d4dc4a6942a1
qiezhichao <zhichao_qzc@sina.com>
2019-06-10 14:30:14 +0800

    启动脚本新增生成dump文件参数

--- qzc-starter-parent/start.sh
+++ qzc-starter-parent/start.sh
@@ -4,4 +4,6 @@ PORT=8989
 SERVICENAME=xingdadata.jar
 SPRINGCONFIG=/home/qzc/xingda/config/application.yml
 
-nohup java -jar -Xms512m -Xmx2048m -XX:PermSize=1024m -XX:MaxNewSize=108m -XX:MaxPermSize=2048m ./$SERVICENAME -Dfile.encoding=UTF-8 --spring.config.location=$SPRINGCONFIG --server.port=$PORT >/dev/null 2>&1 &
+# -XX:+HeapDumpOnOutOfMemoryError : 在发生OutOfMemoryError时，生成dump文件
+# -XX:HeapDumpPath=/var/jvm/logs/ : 生成的dump文件存放路径（/var/jvm/logs/ 需提前创建好）
+nohup java -jar -Xms512m -Xmx2048m -XX:PermSize=1024m -XX:MaxNewSize=108m -XX:MaxPermSize=2048m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/jvm/logs/ ./$SERVICENAME -Dfile.encoding=UTF-8 --spring.config.location=$SPRINGCONFIG --server.port=$PORT >/dev/null 2>&1 &
